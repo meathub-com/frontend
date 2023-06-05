2ff2efb0345a34c98c3c653e89291366fd1eefd7
MTomys <michalltomys@protonmail.ch>
2023-06-05 18:03:33 +0200

    fix formatters

--- meathub-frontend/src/features/auth/api/useLogin.ts
+++ meathub-frontend/src/features/auth/api/useLogin.ts
@@ -1,15 +1,20 @@
-import { axios } from '@/lib/axios.ts';
+import { axios } from "@/lib/axios.ts";
 
 type AuthResponse = {
   authToken: string;
-  refreshToken: string
-}
+  refreshToken: string;
+};
 
 export const useLogin = () => {
-
-  const loginWithEmailAndPassword = async (email: string, password: string): Promise<AuthResponse> => {
+  const loginWithEmailAndPassword = async (
+    email: string,
+    password: string
+  ): Promise<AuthResponse> => {
     const payload = JSON.stringify({ email, password });
-    const response = await axios.post('http:localhost:8080/auth/login', payload);
+    const response = await axios.post(
+      "http:localhost:8080/auth/login",
+      payload
+    );
     return response.data as AuthResponse;
   };
 
